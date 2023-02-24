78a75abdf86bfc779ca7885e2b81d2f6c0c7ed73
Iaroslav Moiseev <yarmoiseev01@gmail.com>
2023-02-24 10:16:50 +0400

    FIX FORMATTING

--- src/Components/UI/Modal/Modal.jsx
+++ src/Components/UI/Modal/Modal.jsx
@@ -3,14 +3,14 @@ import styles from "./Modal.module.css";
 const Modal = (props) => {
   return (
     <div 
-		className={props.active ? `${styles.modal} ${styles.active}` : styles.modal} 
-		onClick={() => props.setActive(false)}
-		>
+    className={props.active ? `${styles.modal} ${styles.active}` : styles.modal} 
+    onClick={() => props.setActive(false)}
+    >
       <div
         className={props.active ? `${styles["modal__content"]} ${styles.active}` : styles["modal__content"]}
         onClick={(e) => e.stopPropagation()}
       >
-				{props.children}
+        {props.children}
       </div>
     </div>
   );
--- src/Components/Users/UsersList.jsx
+++ src/Components/Users/UsersList.jsx
@@ -1,17 +1,19 @@
-import styles from './UsersList.module.css';
+import styles from "./UsersList.module.css";
 
-import Card from '../UI/Card/Card';
+import Card from "../UI/Card/Card";
 
 const UsersList = (props) => {
-	return (
-		<Card className={styles.users}>
-			<ul>
-				{props.users.map(user => (
-				<li key={user.id}>{user.name} ({user.age} years old)</li>
-				))}
-			</ul>
-		</Card>
-	);
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
 };
 
 export default UsersList;
